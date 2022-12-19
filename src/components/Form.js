a5c4a9a783235eadfa4108aa5604d6e0b9839677
joycehwchan <joycehwchan@gmail.com>
2022-12-19 16:05:51 +0900

    set top and bottom text

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,25 +3,29 @@ import React from "react";
 const Form = (props) => {
   return (
     <div className="container mx-auto px-4 md:px-8 pt-8 max-w-screen-lg">
-      <div className="md:flex gap-2">
-        <input
-          type="text"
-          className="mt-2 w-full md:w-1/2 block rounded-md bg-white border-amber-200 focus:border-amber-500 focus:bg-amber-100 focus:ring-0"
-          placeholder="top text"
-        />
-        <input
-          type="text"
-          className="mt-2 w-full md:w-1/2 block rounded-md bg-white border-amber-200 focus:border-amber-500 focus:bg-amber-100 focus:ring-0"
-          placeholder="bottom text"
-        />
-      </div>
-      <button
-        type="submit"
-        className="mt-2 w-full rounded-md bg-amber-400 hover:bg-amber-600 hover:text-white cursor-pointer py-2"
-        onClick={props.getMeme}
-      >
-        Generate a meme!
-      </button>
+      <form onSubmit={props.getMeme}>
+        <div className="md:flex gap-2">
+          <input
+            type="text"
+            name="topText"
+            value={props.meme.topText}
+            onChange={props.addText}
+            className="mt-2 w-full md:w-1/2 block rounded-md bg-white border-amber-200 focus:border-amber-500 focus:bg-amber-100 focus:ring-0"
+            placeholder="Top text"
+          />
+          <input
+            name="bottomText"
+            type="text"
+            value={props.meme.bottomText}
+            onChange={props.addText}
+            className="mt-2 w-full md:w-1/2 block rounded-md bg-white border-amber-200 focus:border-amber-500 focus:bg-amber-100 focus:ring-0"
+            placeholder="Bottom text"
+          />
+        </div>
+        <button className="mt-2 w-full rounded-md bg-amber-400 hover:bg-amber-600 hover:text-white cursor-pointer py-2">
+          Change meme image!
+        </button>
+      </form>
     </div>
   );
 };
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,19 +2,47 @@ import React, { useState } from "react";
 import Form from "./Form";
 
 const Meme = () => {
-  const getMeme = () => {
-    console.log("clicked");
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    image:
+      "https://images.unsplash.com/photo-1671394507023-76c50b824053?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  });
+
+  const getMeme = (event) => {
+    event.preventDefault();
+    const memes = [];
+    const randomNumber = Math.floor(Math.random() * memes.length);
+    // const url = memes[randomNumber].url;
+    const url =
+      "https://images.unsplash.com/photo-1671349507167-3abbeb490244?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80";
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      image: url,
+    }));
+  };
+
+  const addText = (event) => {
+    // console.log(`${event.target.name}: ${event.target.value}`);
+    const { name, value } = event.target;
+
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
+
+    // console.log(text);
   };
 
   return (
     <main className="bg-amber-50">
-      <Form getMeme={getMeme} />
-      <div className="container mx-auto px-4 md:px-8 max-w-screen-lg flex justify-center py-6">
-        <img
-          src="https://images.unsplash.com/photo-1671394507023-76c50b824053?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-          alt="meme"
-          className="w-full rounded-md"
-        />
+      <Form getMeme={getMeme} addText={addText} meme={meme} />
+      <div className="container relative mx-auto px-4 md:px-8 max-w-screen-lg flex justify-center py-6">
+        <p className="meme__text text__top">{meme.topText}</p>
+        <p className="meme__text text__bottom">{meme.bottomText}</p>
+        <img src={meme.image} alt="meme" className="w-full rounded-md" />
       </div>
     </main>
   );
