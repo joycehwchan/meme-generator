d1c80f3d798978d0bef76da3eaa25b3106838446
joycehwchan <joycehwchan@gmail.com>
2022-12-19 17:07:57 +0900

    fetch memes with api

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,26 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "./Form";
 
 const Meme = () => {
+  const [allMemes, setAllMemes] = useState([]);
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
-    image:
-      "https://images.unsplash.com/photo-1671394507023-76c50b824053?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    image: "https://i.imgflip.com/1yxkcp.jpg",
   });
-
-  const getMeme = (event) => {
-    event.preventDefault();
-    const memes = [];
-    const randomNumber = Math.floor(Math.random() * memes.length);
-    // const url = memes[randomNumber].url;
-    const url =
-      "https://images.unsplash.com/photo-1671349507167-3abbeb490244?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80";
-    setMeme((prevMeme) => ({
-      ...prevMeme,
-      image: url,
-    }));
-  };
 
   const addText = (event) => {
     // console.log(`${event.target.name}: ${event.target.value}`);
@@ -36,13 +23,34 @@ const Meme = () => {
     // console.log(text);
   };
 
+  const getMeme = (event) => {
+    event.preventDefault();
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      image: url,
+    }));
+  };
+
+  // fetch memes from api on mount
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
   return (
     <main className="bg-amber-50">
       <Form getMeme={getMeme} addText={addText} meme={meme} />
-      <div className="container relative mx-auto px-4 md:px-8 max-w-screen-lg flex justify-center py-6">
+      <div className="container max-h-[70vh] w-fit relative mx-auto px-4 md:px-8 max-w-screen-lg flex justify-center py-6">
         <p className="meme__text text__top">{meme.topText}</p>
         <p className="meme__text text__bottom">{meme.bottomText}</p>
-        <img src={meme.image} alt="meme" className="w-full rounded-md" />
+        <img
+          src={meme.image}
+          alt="meme"
+          className="w-full rounded-md object-contain min-w-[60vw]"
+        />
       </div>
     </main>
   );
