0248a34bdfc0b33dbec7aa70929f482dbf5afb18
joycehwchan <joycehwchan@gmail.com>
2022-12-19 12:51:50 +0900

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import Form from "./components/Form";
 
 function App() {
   return (
-    <div>
-      <h1 className="font-bold">Hello</h1>
+    <div className="h-screen">
+      <Header />
+      <main className="bg-slate-100">
+        <Form />
+      </main>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+const Header = () => {
+  return (
+    <header className="text-slate-50">
+      <div className="w-screen px-6 md:px-12 py-3 flex items-center justify-between bg-gradient-to-r from-amber-600 to-yellow-600">
+        <div className="flex">
+          <img src={logo} alt="logo" className="w-12" />
+          <h1 className="font-bold text-xl self-center">Meme Generator</h1>
+        </div>
+        <div className="text-xl hover:scale-110 cursor-pointer duration-200">
+          <a href="#">
+            <i className="uil uil-github"></i>
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
